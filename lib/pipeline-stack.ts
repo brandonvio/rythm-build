131403e5e850e72ed9bd0994a590babe9745bdd6
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2020-12-13 13:29:47 -0800

    AWS CDK created codepipeline.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,14 +2,28 @@ import * as cdk from "@aws-cdk/core";
 import * as codebuild from "@aws-cdk/aws-codebuild";
 import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import * as iam from "@aws-cdk/aws-iam";
 import * as events from "@aws-cdk/aws-events";
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const powerUserPolicy = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "BuildPolicy",
+      "arn:aws:iam::aws:policy/PowerUserAccess"
+    );
+
+    const buildRole = new iam.Role(this, "BuildRole", {
+      roleName: "HiddenRoadPipelineRole",
+      assumedBy: new iam.ServicePrincipal("codebuild.amazonaws.com"),
+      managedPolicies: [powerUserPolicy],
+    });
+
     const pipeline = new codepipeline.Pipeline(this, "HiddenRoadPipeline", {
       pipelineName: "hidden-road-pipeline",
+      // role:
     });
 
     const sourceOutput = new codepipeline.Artifact();
@@ -28,7 +42,8 @@ export class PipelineStack extends cdk.Stack {
     });
 
     const buildMicroServProject = new codebuild.PipelineProject(this, "BuildMicroServProject", {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename("./lib/rythm-build.yaml"),
+      buildSpec: codebuild.BuildSpec.fromSourceFilename("rythm-build.yaml"),
+      role: buildRole,
     });
 
     const buildAction = new codepipeline_actions.CodeBuildAction({
