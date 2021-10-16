9c7d6f45fcaee6ea339e4a65d3fa178b638671da
Brandon Vicedomini <brandonvio@outlook.com>
2021-10-15 17:13:11 -0700

    Repurpose project.

---
+++ build/build-stack.ts
@@ -0,0 +1,42 @@
+import * as cdk from 'aws-cdk-lib'
+import * as ecr from 'aws-cdk-lib/aws-ecr'
+import * as cb from 'aws-cdk-lib/aws-codebuild'
+import * as iam from 'aws-cdk-lib/aws-iam'
+import { Construct } from 'constructs'
+
+export class RythmBuildStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+
+        const source = cb.Source.gitHub({
+            owner: 'brandonvio',
+            repo: 'rythm-build',
+            branchOrRef: 'main',
+            webhook: true,
+        })
+
+        const buildRole = new iam.Role(this, 'BuildRole', {
+            roleName: 'rythm-build-role',
+            assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
+        })
+
+        buildRole.addToPolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                resources: ['*'],
+                actions: ['*'],
+            })
+        )
+
+        new cb.Project(this, 'RythmBuildProject', {
+            role: buildRole,
+            projectName: 'rythm-build-project',
+            source: source,
+            buildSpec: cb.BuildSpec.fromSourceFilename('buildspec.yml'),
+            environment: {
+                buildImage: cb.LinuxBuildImage.STANDARD_5_0,
+                privileged: true,
+            },
+        })
+    }
+}
---
+++ build/setup.sh
@@ -0,0 +1,2 @@
+# aws codebuild import-source-credentials --generate-cli-skeleton > credentials.json
+aws codebuild import-source-credentials --cli-input-json file://credentials.json --profile rythm-build
