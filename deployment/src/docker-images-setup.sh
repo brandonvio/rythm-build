06029bb15cfae1ec3e6552c0ba1944007bda7b89
Brandon Vicedomini <brandonvio@outlook.com>
2021-10-21 12:31:28 -0600

    Build.

--- deployment/src/docker-images-setup.sh
+++ deployment/src/docker-images-setup.sh
@@ -1,2 +1,4 @@
+aws ecr get-login-password --region us-west-2 --profile rythm-build | docker login --username AWS --password-stdin 919217319840.dkr.ecr.us-west-2.amazonaws.com
 docker pull python:3.8-slim-buster
-docker tag python:3.8-slim-buster
+docker tag python:3.8-slim-buster 919217319840.dkr.ecr.us-west-2.amazonaws.com/python-38-slim-buster:latest
+docker push 919217319840.dkr.ecr.us-west-2.amazonaws.com/python-38-slim-buster:latest
